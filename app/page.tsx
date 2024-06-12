a29ec374dbe91cc4c2f9023c908991fe7d28798b
Esteban <baab2an@live.fr>
2024-06-12 15:55:49 +0200

    FEAT header + img background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
+import Header from "./components/Header";
 export default function HomePage() {
   return (
     <>
-      <div className="w-full h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-black">Home page</h1>
-      </div>
+      <Header />
     </>
   );
 }
