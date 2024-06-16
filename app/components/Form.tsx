a18bb8a681cde0eb2e480812fe706c648a018d62
Esteban <baab2an@live.fr>
2024-06-16 10:27:29 +0200

    FEAT function pour le formulaire de nouveau posts

---
+++ app/components/Form.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react"
+import { db,storage } from "../db/configFirebase"
+import { doc, collection, setDoc} from "firebase/firestore"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
+import Toasts from "./Toasts"
+import { useRouter } from "next/navigation"
+
+
+interface FormData {
+  title: string
+  description: string
+  author: string
+}
+
+export default function Form() {
+
+
+  const [input, setInput] = useState<FormData>({})
+  const [showToast, setShowToast] = useState<boolean>(false)
+  const [file, setFile] = useState<File | undefined>()
+  const [submit , setSubmit] = useState<boolean>(false)
+
+const router = useRouter();
+
+  useEffect(() => {
+    if (submit) {
+      savePost()
+      router.push('/')
+    }
+  }
+, [submit])
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const {name, value } = e.target
+  setInput ((prevInput) => ({...prevInput, [name]: value}))
+}
+
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  setShowToast(true)
+  if (file){
+    const storageRef = ref(storage, 'images/'+ file?.name)
+    uploadBytes(storageRef, file).then(() => {
+      getDownloadURL(storageRef).then(async (url) => {
+        setInput((prevInput) => ({...prevInput, image: url}))
+        setSubmit(true)
+      })
+    })
+  
+  }
+}
+
+const savePost = async () => {
+  const docRef = doc(collection(db, "posts"))
+  await setDoc(docRef, input)
+}
+
+  return (
+    <div>Form</div>
+  )
+}
--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import Form from "../components/Form";
+
 export default function CreatePage() {
   return (
     <>
       <div className="w-full h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-black">Create page</h1>
+        <div className="p-6 mt-8 lg:w-[35%] md:w-[50%]">
+          <h2 className="text-[30px] text-blue-500 font-black">
+            Ajouter un post
+          </h2>
+          <Form />
+        </div>
       </div>
     </>
   );
