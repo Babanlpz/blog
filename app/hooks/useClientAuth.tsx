7923bde6dfce36b4df95c36e41de633dcb5ba5f3
Esteban <baab2an@live.fr>
2024-06-17 10:52:22 +0200

    FEAT hook pour la connexion et l'inscription

--- app/hooks/useClientAuth.tsx
+++ app/hooks/useClientAuth.tsx
@@ -7,5 +7,30 @@ import { useRouter } from "next/navigation";
 
 const providerGoogle = new GoogleAuthProvider();
 
+const useClientAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isFetch , setIsFetch] = useState(true);
+  const router = useRouter();
 
+  const signUp = async (email: string, password: string) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      setUser(userCredential.user);
+      router.push("/dashboard");
+    }catch (error){
+      console.log(error);
+    }
+  }
+  const signIn = async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      setUser(userCredential.user);
+      router.push("/dashboard");
+    }catch (error){
+      console.log(error);
+    }
+  }
+
+
+}
 
